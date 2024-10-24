b92c8c5a0e5458ae52c1b39aa613b1e54822f687
idmaksim <i@dmaksim.ru>
2024-10-24 16:31:25 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+
+  const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT token',
+      in: 'header',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      persistAuthorization: true,
+      defaultModelsExpandDepth: -1,
+      docExpansion: 'none',
+      preloadModels: false,
+      tryItOutEnabled: false,
+      syntaxHighlight: false,
+    },
+  });
+  await app.listen(port);
+}
+bootstrap();
