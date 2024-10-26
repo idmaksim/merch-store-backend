fab4672cc2cc9ca99f0d1da5ad45c64344658805
idmaksim <i@dmaksim.ru>
2024-10-26 18:34:26 +0300

    made new migratins and add "pladce" field to concert

--- prisma/seeds/concert.seed.ts
+++ prisma/seeds/concert.seed.ts
@@ -7,31 +7,37 @@ export async function seedConcerts(prisma: PrismaService) {
       title: 'Motionless in White',
       images: ['motionless1.jpeg', 'motionless2.jpg', 'motionless3.jpg'],
       date: '2024-12-25',
+      place: 'Калифорния',
     },
     {
       title: 'Slipknot',
       images: ['slipknot1.jpg', 'slipknot2.jpg', 'slipknot3.jpeg'],
       date: '2024-12-26',
+      place: 'Техас',
     },
     {
       title: 'Slaughter to Prevail',
       images: ['slaughter1.jpg', 'slaughter2.jpg', 'slaughter3.jpg'],
       date: '2024-12-27',
+      place: 'Флорида',
     },
     {
       title: 'Bullet for My Valentine',
       images: ['bullet1.jpg', 'bullet2.jpg', 'bullet3.jpg'],
       date: '2024-12-28',
+      place: 'Нью-Йорк',
     },
     {
       title: 'Element Eighty',
       images: ['element1.jpg', 'element2.jpg', 'element3.jpg'],
       date: '2024-12-29',
+      place: 'Иллинойс',
     },
     {
       title: 'Linkin Park',
       images: ['linkin1.jpg', 'linkin2.jpg', 'linkin3.jpg'],
       date: '2024-12-30',
+      place: 'Пенсильвания',
     },
   ];
   for (const concert of concerts) {
--- src/modules/concert/concert.controller.ts
+++ src/modules/concert/concert.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ConcertService } from './concert.service';
 import { ApiTags } from '@nestjs/swagger';
 
